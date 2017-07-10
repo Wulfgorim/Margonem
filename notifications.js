cdcf7fbec2d72475d41df17743ec17776a632790
Wulfgorim <Wulfgorim@users.noreply.github.com>
2017-07-10 19:03:18 +0200

    Dodanie g.load

--- notifications.js
+++ notifications.js
@@ -1,7 +1,9 @@
-setInterval(function() {
-if (hero.nick == "Wulfgorim") {
-  console.log("Działa");
-} else {
-  console.log("Nie wiem skąd masz dodatek, ale weź wypierdalaj");
-}
-}, 2000);
+g.loadQueue.push({
+	fun: function() {
+		if (hero.nick == "Wulfgorim") {
+			console.log("Działa");
+		} else {
+			console.log("Nie wiem skąd masz dodatek, ale weź wypierdalaj");
+		}
+	}
+});
