8fa300b92389e2eace5c3f57c033bcf40a009e05
Wulfgorim <Wulfgorim@users.noreply.github.com>
2017-07-10 19:10:53 +0200

    Delete notifications.js

--- notifications.js
+++
@@ -1,9 +0,0 @@
-g.loadQueue.push({
-	fun: function() {
-		if (hero.nick == "Wulfgorim") {
-			console.log("Działa");
-		} else {
-			console.log("Nie wiem skąd masz dodatek, ale weź wypierdalaj");
-		}
-	}
-});
